98da0dc211f4716aa3bd89cdf7b76bb8c7bea37f
Paul Campbell <paul@tito.io>
2023-01-16 15:10:41 +0000

    Starting to look pretty good!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thisnextthing.com',
+  integrations: [tailwind(), vue()]
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"light-blue": "#7badee",
+				"royal-blue": "#002658",
+				"warm-blue": "#0056AA",
+				"world-blue": "#0170bc"
+			}
+		},
+	},
+	plugins: [],
+}
