61cf2479b769d889bcdcad762e61eba5979c1e72
Paul Campbell <paul@tito.io>
2023-01-22 13:20:48 +0000

    Refactor the heading styles as per @toolmantim’s feedback (#13)

--- src/components/TickTockSection.vue
+++ src/components/TickTockSection.vue
@@ -13,14 +13,14 @@ export default {
 <style scoped>
   section {  max-width: 80ch; @apply md:pb-8 }
 
-  section.tick, section.tock { @apply bg-opacity-80 -mt-4 md:-mt-8 md:rounded-xl overflow-hidden  }
+  section.tick, section.tock { @apply bg-opacity-80 -mt-4 md:-mt-8 rounded-xl overflow-hidden  }
 
   .tick { 
-    @apply bg-light-blue md:ml-8 text-royal-blue md:rounded-l-xl md:rotate-1;
+    @apply bg-light-blue md:ml-8 text-royal-blue rounded-l-xl rotate-1;
    }
 
   .tock { 
-    @apply bg-royal-blue md:mr-8 text-white md:rounded-r-xl md:-rotate-1;
+    @apply bg-royal-blue md:mr-8 text-white rounded-r-xl -rotate-1;
   }
 
   .tock h2, .tock a {
