6c9c1cf9df8900b0fa012d08da345de646c8f0bf
Paul Campbell <paul@tito.io>
2023-01-24 23:46:41 +0000

    Really get the royal blue text

--- src/components/StarterCityChooser.vue
+++ src/components/StarterCityChooser.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="mb-4 flex divide-x w-full block rounded overflow-hidden">
     <li v-for="city in cities" class="bg-royal-blue block text-white flex-1 flex items-center justify-center"
-        :class="{'bg-blue-100 text-royal-blue': starterCity === city}">
+        :class="{'bg-blue-100': starterCity === city}">
       <label class="inline-flex items-center gap-2 cursor-pointer p-2 md:px-4 whitespace-nowrap w-full flex items-center justify-center">
         <input type="radio" class="sr-only" name="starterCity" v-model="starterCity" @click="starterCity = city" :value="city">
-        <span :class="{'font-bold': starterCity === city}">{{cityEmojis[city]}} {{city}}</span>
+        <span :class="{'font-bold text-royal-blue': starterCity === city}">{{cityEmojis[city]}} {{city}}</span>
       </label>
     </li>
   </ul>
