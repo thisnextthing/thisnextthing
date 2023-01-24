0968ab90d42e0eb9b1db0539025cdb8f9845c98c
Paul Campbell <paul@tito.io>
2023-01-24 14:44:02 +0000

    Prevent the form in the background from leaking through

--- src/components/MailingListSignup.vue
+++ src/components/MailingListSignup.vue
@@ -1,22 +1,24 @@
 <template>
   <form @submit.prevent.stop="submit">
-    <div class="flex items-center justify-center relative mt-4 p-4 transition-all" :class="{'pt-[7rem]': consented}">
-      <div class="absolute top-0 left-0 right-0 rounded p-4 flex flex-col gap-4 items-center justify-center transition-all"
+    <div class="bg-white border-b flex items-center justify-center relative mt-4 p-4 transition-all" :class="{'pt-[8rem]': consented}">
+      <div class="absolute top-0 left-0 right-0 rounded flex flex-col gap-4 items-center justify-center transition-all"
            :class="{
-            'bg-white opacity-95 bottom-0': !consented
+            'bg-white bg-opacity-95 bottom-0': !consented
            }">
-        <h2 for="email" class="block text-md font-light text-center">Sign up for updates</h2>
-        <fieldset>
-          <legend class="sr-only">Consent to sign up</legend>
-          <div class="relative flex items-start">
-            <div class="flex h-5 items-center">
-              <input @click="hello" v-model="consented" id="consented" aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 focus:ring-indigo-500" />
+        <div class="flex flex-col gap-4 bg-white p-4 border-t border-b">
+          <h2 for="email" class="block text-md font-light text-center">Sign up for updates</h2>
+          <fieldset>
+            <legend class="sr-only">Consent to sign up</legend>
+            <div class="relative flex items-start">
+              <div class="flex h-5 items-center">
+                <input @click="hello" v-model="consented" id="consented" aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 focus:ring-indigo-500" />
+              </div>
+              <div class="ml-3 text-sm">
+                <label for="consented">I consent to being contacted by email for information about This Next Thing</label>
+              </div>
             </div>
-            <div class="ml-3 text-sm">
-              <label for="consented">I consent to being contacted by email for information about This Next Thing</label>
-            </div>
-          </div>
-        </fieldset>
+          </fieldset>
+        </div>
       </div>
       <tito-register-interest event="this-next-thing/2023"/>
     </div>
