e363b6ffd1f2d53b8abbe61d2118e184cb3ede60
Paul Campbell <paul@tito.io>
2024-01-11 09:44:32 +0000

    Redirect to tito (#50)
    * locations
    
    * Redirect booking page to Tito
    
    (for now while we figure out the widget bug)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thisnextthing.com/',
-  integrations: [tailwind(), vue({
-    template: {
-      compilerOptions:{
-        isCustomElement: (tag) => ['tito-register-interest', 'tito-widget', 'tito-button'].includes(tag)
-      }
-    }
-  })]
+  site: "https://thisnextthing.com/",
+  integrations: [
+    tailwind(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            ["tito-register-interest", "tito-widget", "tito-button"].includes(
+              tag,
+            ),
+        },
+      },
+    }),
+  ],
+  redirects: {
+    "/2024/book": "https://ti.to/this-next-thing/2024",
+  },
 });
